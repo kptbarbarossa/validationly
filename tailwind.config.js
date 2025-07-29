fec0303221655e0709d3dad0ba2e9e2c0ac637bc
kptbarbarossa <mustafakoklu@gmail.com>
2025-07-29 20:35:49 +0300

    Restore original design: Add Tailwind CSS with proper configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
       },
     },
   },
