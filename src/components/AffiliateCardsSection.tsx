11f3cba8729ce2d6c1f69cdf3a9ecaa52ca24f08
kptbarbarossa <mustafakoklu@gmail.com>
2025-09-02 22:31:50 +0300

    Fix affiliate cards grid layout to 3 columns max

--- src/components/AffiliateCardsSection.tsx
+++ src/components/AffiliateCardsSection.tsx
@@ -30,7 +30,7 @@ const AffiliateCardsSection: React.FC<AffiliateCardsSectionProps> = ({
       </div>
 
       {/* Enhanced Cards Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 sm:gap-8 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         {cardsToShow.map((card) => (
           <AffiliateCard
             key={card.id}
